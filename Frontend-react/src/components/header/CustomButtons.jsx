a68764c5ee40e4bd918301b7b662415f6881b4ab
omkar1103 <115880514+omkar1103@users.noreply.github.com>
2025-02-28 13:33:24 +0530

    Login page implementd and Cart also implemented

--- Frontend-react/src/components/header/CustomButtons.jsx
+++ Frontend-react/src/components/header/CustomButtons.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Box, Typography, Button, styled } from '@mui/material';
-import { ShoppingCart } from '@mui/icons-material';
+import React, { useState } from "react";
+import { Box, Typography, Button, styled } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import LoginDialog from "../login/LoginDialog";
 
 const Container = styled(Box)`
   display: flex;
@@ -23,22 +24,37 @@ const Wrapper = styled(Box)`
   margin: 0 3% 0 auto;
   align-items: center;
 
-  & > button, & > p, & > div {
+  & > button,
+  & > p,
+  & > div {
     margin-right: 40px;
     font-size: 14px;
   }
 `;
 
 const CustomButtons = () => {
+  const [open, setOpen] = useState(false);
+
+  const openDialog = () => {
+    setOpen(true);
+  };
+
+  const closeDialog = () => {
+    setOpen(false);
+  };
+
   return (
     <Wrapper>
-      <LoginButton variant="contained">Login</LoginButton>
+      <LoginButton variant="contained" onClick={openDialog}>
+        Login
+      </LoginButton>
       <Typography style={{ marginTop: 3, width: 135 }}>Become a Seller</Typography>
       <Typography style={{ marginTop: 3 }}>More</Typography>
       <Container>
         <ShoppingCart style={{ marginRight: 8 }} />
         <Typography>Cart</Typography>
       </Container>
+      <LoginDialog open={open} handleClose={closeDialog} />
     </Wrapper>
   );
 };
