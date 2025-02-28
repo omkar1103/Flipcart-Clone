0c4e20677fdf013133adf4fa05c8ca9b670705a3
omkar1103 <115880514+omkar1103@users.noreply.github.com>
2025-02-28 15:03:26 +0530

    Successfully connected with MongoDb

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -1,16 +1,14 @@
 import mongoose from 'mongoose';
 
 const Connection = async () => {
-    // const URL = `mongodb://${username}:${password}@ecommerceweb-shard-00-00.fdvft.mongodb.net:27017,ecommerceweb-shard-00-01.fdvft.mongodb.net:27017,ecommerceweb-shard-00-02.fdvft.mongodb.net:27017/ECOMMERCE?ssl=true&replicaSet=atlas-8a6bhp-shard-0&authSource=admin&retryWrites=true&w=majority`;
-    // const URL = `mongodb://${username}:${password}@ecommerce-shard-00-00.fdvft.mongodb.net:27017,ecommerce-shard-00-01.fdvft.mongodb.net:27017,ecommerce-shard-00-02.fdvft.mongodb.net:27017/ECOMMERCE?ssl=true&replicaSet=atlas-ilaj5d-shard-0&authSource=admin&retryWrites=true&w=majority`;
-    const URL = `mongodb://test-yt:B3Rg7YjKzQNq9EYc@test-shard-00-00.5ouyh.mongodb.net:27017,test-shard-00-01.5ouyh.mongodb.net:27017,test-shard-00-02.5ouyh.mongodb.net:27017/TEST?ssl=true&replicaSet=atlas-k076xn-shard-0&authSource=admin&retryWrites=true&w=majority&appName=test`;
+    const URL = `mongodb://test-yt:B3Rg7YjKzQNq9EYc@test-shard-00-00.5ouyh.mongodb.net:27017/testdb?ssl=true&replicaSet=atlas-k076xn-shard-0&authSource=admin&retryWrites=true&w=majority&appName=test`;
+    
     try {
-        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true,  });
-        console.log('Database Connected Succesfully');
-    } catch(error) {
-        console.log('Error: ', error.message);
+        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
+        console.log('✅ Database Connected Successfully');
+    } catch (error) {
+        console.log('❌ Error: ', error.message);
     }
-
 };
 
 export default Connection;
--- Backend/index.js
+++ Backend/index.js
@@ -1,23 +1,10 @@
-
 import express from 'express';
-
-import dotenv from 'dotenv';
-
 import Connection from './database/db.js';
-import DefaultData from './default.js';
 
-
-
-dotenv.config();
 const app = express();
-
 const PORT = 8000;
 
-const username = process.env.DB_USERNAME;
-const password = process.env.DB_PASSWORD;
+// Call MongoDB connection
+Connection();
 
-Connection(username, password);
-
-app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
-DefaultData();
-
+app.listen(PORT, () => console.log(`🚀 Server is running successfully on PORT ${PORT}`));
